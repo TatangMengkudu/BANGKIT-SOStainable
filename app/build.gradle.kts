3e5451b88a6867867404b0f9f627c338e684beb3
Fannannf <fananfalahi46@gmail.com>
2024-06-04 02:12:45 +0700

    add page detail

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //library CardImage
+    implementation(libs.androidx.cardview)
 }

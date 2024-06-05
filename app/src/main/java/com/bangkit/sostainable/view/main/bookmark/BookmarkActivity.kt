21ed69da188abf0d3893eeace5cec70a866e78e9
Rizal Pratama <zalllrizalll012@gmail.com>
2024-06-05 15:33:21 +0700

    [CREATE] Improved the create activity page

---
+++ app/src/main/java/com/bangkit/sostainable/view/main/bookmark/BookmarkActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.sostainable.view.main.bookmark
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bangkit.sostainable.R
+
+class BookmarkActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bookmark)
+    }
+}

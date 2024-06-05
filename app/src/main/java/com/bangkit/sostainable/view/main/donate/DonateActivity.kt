c7f14599e0601156e1c60f974b001313c5386b47
Fannannf <fananfalahi46@gmail.com>
2024-06-05 14:32:28 +0700

    [Create,Donate] Add Page and some appearences

--- app/src/main/java/com/bangkit/sostainable/view/main/donate/DonateActivity.kt
+++ app/src/main/java/com/bangkit/sostainable/view/main/donate/DonateActivity.kt
@@ -1,21 +1,26 @@
 package com.bangkit.sostainable.view.main.donate
 
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bangkit.sostainable.R
+import com.bangkit.sostainable.databinding.ActivityDonateBinding
 
 class DonateActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDonateBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_donate)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityDonateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rgPaymentMethods.setOnCheckedChangeListener { group, checkedId ->
+            val radioButton = group.findViewById(checkedId) as RadioButton
+            Toast.makeText(this,radioButton.text,Toast.LENGTH_SHORT).show()
         }
     }
 }

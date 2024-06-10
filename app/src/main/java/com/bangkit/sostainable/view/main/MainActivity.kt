b590ffeb6066794f48c9febceaba8668205a2664
Rizal Pratama <zalllrizalll012@gmail.com>
2024-06-10 11:39:28 +0700

    [PROFILE] Improve the layout of the fragment profile

--- app/src/main/java/com/bangkit/sostainable/view/main/MainActivity.kt
+++ app/src/main/java/com/bangkit/sostainable/view/main/MainActivity.kt
@@ -3,11 +3,8 @@ package com.bangkit.sostainable.view.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupWithNavController
 import com.bangkit.sostainable.R
 import com.bangkit.sostainable.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -21,17 +18,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
         val navView: BottomNavigationView = binding.bottomNavigationView
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-
-        val appBarConfiguration = AppBarConfiguration.Builder(
-            R.id.navigation_home,
-            R.id.navigation_create,
-            R.id.navigation_profile
-        ).build()
 
         NavigationUI.setupWithNavController(navView, navController)
     }

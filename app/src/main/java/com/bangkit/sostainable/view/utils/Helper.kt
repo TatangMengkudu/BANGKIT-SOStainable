6d0208c0825742ecd6b074a56d56ef55e1796c53
Rizal Pratama <zalllrizalll012@gmail.com>
2024-06-19 19:56:57 +0700

    [ALL LAYOUTS] Improve page layout and layout activities

--- app/src/main/java/com/bangkit/sostainable/view/utils/Helper.kt
+++ app/src/main/java/com/bangkit/sostainable/view/utils/Helper.kt
@@ -10,7 +10,7 @@ import java.util.TimeZone
 @RequiresApi(Build.VERSION_CODES.O)
 fun dateFormat(created: String): String {
     val instant = Instant.parse(created)
-    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    val formatter = DateTimeFormatter.ofPattern("dd MMM yyyy")
         .withZone(ZoneId.of(TimeZone.getDefault().id))
     return formatter.format(instant)
 }
